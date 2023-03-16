40ce9240bd5c1947b5a915943b1302f00f49c200
caozhongpa <yzfff6@outlook.com>
2023-03-16 11:16:16 +0800

    fix: delete useless codes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,8 +2,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
-import sphinx_rtd_theme
-
 # -- Project information -----------------------------------------------------
 project = 'spaTrack_tutorials'
 copyright = '2023, BGI'
@@ -14,30 +12,10 @@ release = '0.0.1'
 extensions = [
     'nbsphinx',
     'myst_parser',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.intersphinx',
 ]
 
-autosummary_generate = True
-autodoc_member_order = 'bysource'
-napoleon_include_init_with_doc = False
-napoleon_numpy_docstring = True
-napoleon_use_rtype = True
-napoleon_use_param = True
-
-templates_path = ['_templates']
-exclude_patterns = ['_build', '**.ipynb_checkpoints']
-
-intersphinx_mapping={
-    'numpy': ('https://numpy.org/doc/stable/',None),
-    'anndata': ('https://anndata.readthedocs.io/en/latest/',None),
-    
-}
-
 # -- Options for HTML output -------------------------------------------------
-
+import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,8 +1,3 @@
-.. spaTrack_tutorials documentation master file, created by
-   sphinx-quickstart on Wed Mar 15 18:23:38 2023.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
 Welcome to spaTrack_tutorials's documentation!
 ==============================================
 
@@ -16,6 +11,3 @@ Welcome to spaTrack_tutorials's documentation!
 
    notebooks/Apply_spaTrack_on_spatial_data_v2
    notebooks/spaTracks_multiple_origins_processing
-   notebooks/more_time_transfer_matrix
-
-
